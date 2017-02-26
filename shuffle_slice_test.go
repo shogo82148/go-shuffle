387e93fd6a18d8957162e77028ec4f28cfd118a5
Ichinose Shogo <shogo82148@gmail.com>
2017-02-26 16:04:49 +0900

    add benchmark foe suffling structs

--- shuffle_slice_test.go
+++ shuffle_slice_test.go
@@ -4,6 +4,7 @@ package shuffle
 
 import (
 	"fmt"
+	"math/rand"
 	"reflect"
 	"sort"
 	"testing"
@@ -42,6 +43,32 @@ func BenchmarkSlice(b *testing.B) {
 			a := make([]int, n)
 			for i := 0; i < b.N; i++ {
 				Ints(a)
+			}
+		})
+
+	}
+}
+
+func BenchmarkStructs(b *testing.B) {
+	type Struct struct {
+		data [1024]int8
+	}
+	for _, n := range []int{1, 10, 100, 1000, 10000} {
+
+		b.Run(fmt.Sprintf("shuffle %d", n), func(b *testing.B) {
+			a := make([]Struct, n)
+			for i := 0; i < b.N; i++ {
+				Slice(a)
+			}
+		})
+
+		b.Run(fmt.Sprintf("perm and move %d", n), func(b *testing.B) {
+			s1 := make([]Struct, n)
+			s2 := make([]Struct, n)
+			for i := 0; i < b.N; i++ {
+				for i, j := range rand.Perm(n) {
+					s2[i] = s1[j]
+				}
 			}
 		})
 
