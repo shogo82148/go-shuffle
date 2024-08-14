9b99aa9be83a148a93cb34aabfd66d5598aa376b
ICHINOSE Shogo <shogo82148@gmail.com>
2024-08-14 20:04:04 +0900

    drop Int64Slice

--- interface.go
+++ interface.go
@@ -2,7 +2,6 @@ package shuffle
 
 import (
 	"math/rand/v2"
-	"sort"
 )
 
 // Interface is a type, typically a collection, that satisfies shuffle.Interface can be
@@ -14,21 +13,8 @@ type Interface interface {
 	Swap(i, j int)
 }
 
-// Int64Slice attaches the methods of Interface to []int64, sorting in increasing order.
-type Int64Slice []int64
-
-func (p Int64Slice) Len() int           { return len(p) }
-func (p Int64Slice) Less(i, j int) bool { return p[i] < p[j] }
-func (p Int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-
-// SortInt64s sorts a slice of int64s in increasing order.
-func SortInt64s(a []int64) { sort.Sort(Int64Slice(a)) }
-
 // Ints shuffles a slice of ints.
 func Ints(a []int) { shuffleSlice(a) }
-
-// Int64s shuffles a slice of int64s.
-func Int64s(a []int64) { shuffleSlice(a) }
 
 // Float64s shuffles a slice of float64s.
 func Float64s(a []float64) { shuffleSlice(a) }
